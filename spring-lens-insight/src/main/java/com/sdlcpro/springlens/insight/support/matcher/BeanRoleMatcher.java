9e4d753093e1f10a802b0c6390898b35989d1984
mamun <aalmamun295@gmail.com>
2026-07-12 17:00:49 +0600

    implement the bean role matcher to match the spring bean-role by matcher engine

---
+++ spring-lens-insight/src/main/java/com/sdlcpro/springlens/insight/support/matcher/BeanRoleMatcher.java
@@ -0,0 +1,25 @@
+package com.sdlcpro.springlens.insight.support.matcher;
+
+import com.sdlcpro.springlens.insight.bean.BeanRoleProvider;
+import com.sdlcpro.springlens.matcher.Matcher;
+import com.sdlcpro.springlens.model.bean.BeanRole;
+import org.springframework.util.CollectionUtils;
+
+import java.util.EnumSet;
+
+public class BeanRoleMatcher<T extends BeanRoleProvider> implements Matcher<T> {
+    private final EnumSet<BeanRole> beanRoles;
+
+    public BeanRoleMatcher(EnumSet<BeanRole> beanRoles) {
+        this.beanRoles = beanRoles != null
+                ? EnumSet.copyOf(beanRoles)
+                : EnumSet.noneOf(BeanRole.class);
+    }
+
+    @Override
+    public boolean matches(T context) {
+        return context != null
+                && !CollectionUtils.isEmpty(this.beanRoles)
+                && this.beanRoles.contains(context.getBeanRole());
+    }
+}
