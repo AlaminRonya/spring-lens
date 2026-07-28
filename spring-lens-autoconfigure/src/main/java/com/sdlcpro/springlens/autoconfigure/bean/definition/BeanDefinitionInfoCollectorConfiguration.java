7dc08334d981e57bbfdf8a02938fc6ef0b6665d3
mamun <aalmamun295@gmail.com>
2026-07-28 09:36:03 +0600

    add bean definition collector configuration

---
+++ spring-lens-autoconfigure/src/main/java/com/sdlcpro/springlens/autoconfigure/bean/definition/BeanDefinitionInfoCollectorConfiguration.java
@@ -0,0 +1,38 @@
+package com.sdlcpro.springlens.autoconfigure.bean.definition;
+
+import com.sdlcpro.springlens.annotation.SpringLensInternalComponent;
+import com.sdlcpro.springlens.autoconfigure.bean.SpringLensBeanProperties;
+import com.sdlcpro.springlens.insight.bean.BeanInfoCollectorSettings;
+import com.sdlcpro.springlens.insight.bean.definition.BeanDefinitionInfoCollector;
+import com.sdlcpro.springlens.listener.bean.BeanDefinitionInfoCollectListener;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+
+@SpringLensInternalComponent
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+class BeanDefinitionInfoCollectorConfiguration {
+
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public BeanDefinitionInfoCollector beanDefinitionInfoCollector(
+            ApplicationContext context, SpringLensBeanProperties properties,
+            ObjectProvider<BeanDefinitionInfoCollectListener> beanDefinitionInfoCollectListenerProvider) {
+
+        SpringLensBeanProperties.Include include = properties.getInclude();
+        SpringLensBeanProperties.Exclude exclude = properties.getExclude();
+
+        var settings = new BeanInfoCollectorSettings(
+                include.isRoleInfra(),
+                include.isToolInternal(),
+                exclude.getPackagePatterns(),
+                exclude.getClasses()
+        );
+
+        return new BeanDefinitionInfoCollector(context, settings, beanDefinitionInfoCollectListenerProvider);
+    }
+}
